0889e16a226b295b9034abac4ad01d19bbfcea0b
stormrageqb <stormrage.qb@gmail.com>
2023-08-29 15:57:33 +0100

    about component

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,7 @@
+export { default as About } from "./about/About";
 export { default as Features } from "./features/Features";
 export { default as Footer } from "./footer/Footer";
 export { default as Header } from "./header/Header";
 export { default as Possibility } from "./possibility/Possibility";
 export { default as Faq } from "./faq/Faq";
-export { default as About } from "./about/About";
 export { default as Contact } from "./contact/Contact";
